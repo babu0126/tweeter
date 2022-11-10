a3d1228de1732615c6f2e0658673c9d02826f6f8
David Wang <18davidwang@gmail.com>
2022-11-09 16:25:51 -0800

    Implementing the header, tweet header, tweet article, layout

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#tweet-text').on('input', function() {
+    const maxTweetLength = 140;
+    let inputCharLength = $(this).val().length;
+    let siblingCounter = $(this).siblings().find('.counter');  
+    // $(this).siblings('counter') WHY DOESN'T WORK
+
+    // console.log(inputCharLength);
+    console.log(siblingCounter);
+
+    if (inputCharLength > maxTweetLength) {
+      siblingCounter.addClass('overLengthLimit');
+    } else if (inputCharLength <= maxTweetLength) {
+      siblingCounter.removeClass('overLengthLimit');
+    }
+    $(siblingCounter).val(maxTweetLength - inputCharLength);
+  });
+}); 
